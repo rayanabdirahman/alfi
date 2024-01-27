1aaf0c4ab8e49e380bbec46d1ed0025f19b87c62
rayanabdirahman <rayanmabdirahman@gmail.com>
2024-01-27 22:07:03 +0000

    Intial remix, tailwind and shadcn configuration

---
+++ app/routes/_index.tsx
@@ -0,0 +1,18 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    // <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div >
+      <h1 className="text-4xl">Welcome to Remix</h1>
+    </div>
+  );
+}
